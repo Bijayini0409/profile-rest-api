e5f246996f20147a7f4d2faecab631075f3f4636
Bijayini0409 <bijayini.sethi09@gmail.com>
2021-02-08 11:29:11 +0530

    Added POST, PUT,PATCH, DELETE

---
+++ profiles_api/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+class HelloSerializer(serializers.Serializer):
+    """serializing a namefiled for testing our APIView"""
+    name=serializers.CharField(max_length=10)
